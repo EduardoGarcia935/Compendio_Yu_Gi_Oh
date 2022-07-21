df935298910f818f08214b3145c4329780307258
EduardoGarcia935 <eduardo.agarcia117@gmail.com>
2022-07-21 16:23:55 -0300

    Finalizando a interface, juntamente com a criação da função adicionarCarta no JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,47 @@
-fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?language=pt')
-    .then(resp => resp.json())
-    .then( (json)=>{
-        console.log(json)
-    })
+const btnSubmit = document.querySelector('.submit')
+const inputNome = document.querySelector('.nome')
+const inputTipo = document.querySelector('.selection')
+const area_cartas = document.querySelector('.area-cartas')
+
+
+btnSubmit.addEventListener('click' , e=>{
+
+    if(!inputNome.value || !inputTipo.value){
+        return
+    }
+
+    criarCard(inputNome.value , inputTipo.value , 'https://storage.googleapis.com/ygoprodeck.com/pics/27551.jpg')
+})
+
+
+function criarCard(nome , descricao , src){
+
+    area_cartas.innerHTML = null
+
+    const divPai = document.createElement('div')
+    divPai.setAttribute('class' , 'card')
+
+    const divImg = document.createElement('div')
+    divImg.setAttribute('class' , 'img_container')
+    const img = document.createElement('img')
+    img.setAttribute('src' , src)
+
+    divImg.appendChild(img)
+
+    const divText = document.createElement('div')
+    divText.setAttribute('class' , 'text_container')
+    const h2 = document.createElement('h2')
+    h2.innerHTML = nome
+    const p = document.createElement('p')
+    p.innerHTML = descricao
+
+    divText.appendChild(h2)
+    divText.appendChild(p)
+
+    divPai.appendChild(divImg)
+    divPai.appendChild(divText)
+
+    area_cartas.appendChild(divPai)
+}
+
 
